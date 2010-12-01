60bece9ac026531a2edfba6b9ca817952584631f
prakashmurthy <prakblr@yahoo.com>
2010-12-01 14:57:30 -0700

    made logo a helper method - exercise 5.5.1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,8 @@
 module ApplicationHelper
   
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :title => "Will this show?", :class => "round")
+  end
   # Return a title on a per-page basis.
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
