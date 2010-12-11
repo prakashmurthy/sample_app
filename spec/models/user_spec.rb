582253c211a782fc28e8cd75529f3f11be788879
prakashmurthy <prakblr@yahoo.com>
2010-12-11 15:44:11 -0700

    Add test to verify uniqueness of email ID

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -40,4 +40,11 @@ describe User do
       invalid_email_user.should_not be_valid
     end
   end
+  
+  it "should reject duplicate email addresses" do
+    # Put a user with given emai address into the database
+    User.create!(@attr)
+    user_with_duplicate_email = User.new(@attr)
+    user_with_duplicate_email.should_not be_valid
+  end
 end
