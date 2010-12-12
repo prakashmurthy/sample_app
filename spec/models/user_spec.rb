aaa94eb6fd76ffc0f69003e1d58e27485917b0e0
prakashmurthy <prakblr@yahoo.com>
2010-12-12 12:33:04 -0700

    include test for existence of an encrypted_password attribute + 'rails generate migration add_password_to_users encrypted_password:string'

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -81,5 +81,15 @@ describe User do
       hash = @attr.merge(:password => long, :password_confirmation => long)
       User.new(hash).should_not be_valid
     end
+    
+    describe "password encryption" do
+      
+      before (:each) do
+        @user = User.create!(@attr)
+      end
+      it "should have an encrypted password attribute" do
+        @user.should respond_to(:encrypted_password)
+      end
+    end
   end
 end
