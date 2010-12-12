ee560da4af8719a45b0003a9124a0cdbefb3f99b
prakashmurthy <prakblr@yahoo.com>
2010-12-12 12:38:11 -0700

    'rake db:migrate' | 'rake db:test:prepare' | 'annotate'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 # == Schema Information
-# Schema version: 20101205165857
+# Schema version: 20101212192925
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
 class User < ActiveRecord::Base
